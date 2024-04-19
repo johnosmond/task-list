8dd2c54872a3c82f4d07e08256ea2dbecc9c8355
John Osmond <john@jtech10.com>
2024-04-19 17:54:30 -0400

    Added app layout. Added two blade templates (index and show).

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title', 'Tasks List')
+
+@section('content')
+    <p>Welcome to the main page</p>
+    <div>
+        {{-- @if (count($tasks)) --}}
+        @forelse ($tasks as $task)
+            <div>
+                <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+            </div>
+        @empty
+            <div>There are no tasks</div>
+        @endforelse
+        {{-- @endif --}}
+    </div>
+@endsection
